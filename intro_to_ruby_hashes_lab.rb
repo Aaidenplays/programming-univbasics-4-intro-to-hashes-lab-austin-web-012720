5f24701821c0a21f233ad8b335dd1a5f9a9dfa40
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2019-12-29 20:32:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,18 @@
 def new_hash
-  # return an empty hash
+  hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash = {
+    sample: "i am a sample",
+    more: "more sample",
+    sally: "I am a piggy"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer[:name] = 'Grace Hopper'
 end
 
 def id_generator
